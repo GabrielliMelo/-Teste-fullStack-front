02a01f288ea3125b3c8017daa959722926697ee3
Gabrielli5047 <gabrielli.meloo16@gmail.com>
2022-07-14 13:28:26 -0300

    chose: main page structure

--- src/Pages/Main/App.js
+++ src/Pages/Main/App.js
@@ -1,17 +1,32 @@
+import { useEffect, useState } from "react";
 import "./style.css";
 import HeaderPoke from "../../Components/HeaderPoke";
 import CardPoke from "../../Components/CardPoke";
+// import api from "../../Services/api";
+import pokemonstTeste from "../../Services/MokePokemons";
 
 function App() {
+  const [pokemonsData, setPokemonsData] = useState(pokemonstTeste);
+  console.log(pokemonsData);
+
   return (
     <div className="App">
       <HeaderPoke />
-      <div className="container-cards displayFlex">
-        <CardPoke />
-        <CardPoke />
-        <CardPoke />
-        <CardPoke />
-      </div>
+      <nav className="displayFlex">
+        <div className="container-cards displayFlex">
+          {pokemonsData.map((pokemon) => {
+            return (
+              <CardPoke
+                name={pokemon.name}
+                species={pokemon.species.name}
+                sprintes={pokemon.sprites.back_default}
+                weight={pokemon.weight}
+                height={pokemon.height}
+              />
+            );
+          })}
+        </div>
+      </nav>
     </div>
   );
 }
