692038ff809ef8061f1ca21bd9392cfe622f80d5
Gabrielli5047 <gabrielli.meloo16@gmail.com>
2022-07-14 08:59:42 -0300

    chore: card test structure

--- src/Components/CardPoke/index.js
+++ src/Components/CardPoke/index.js
@@ -1,9 +1,23 @@
-import './style.css';
+import "./style.css";
+import imgTeste from "../../Assets/charmeleon-teste.jpeg";
 
 function CardPoke() {
   return (
     <div className="cardPoke">
-    
+      <h2>Charmeleon</h2>
+      <img src={imgTeste} alt="pokemon" />
+      {/* especie altura peso */}
+      <nav className="heigth-weight">
+        <h3>
+          <b>Especie:</b> teste
+        </h3>
+        <span>
+          <b>Altura:</b> 0.7m
+        </span>
+        <span>
+          <b>Peso:</b> 69kg
+        </span>
+      </nav>
     </div>
   );
 }
--- src/Pages/Main/App.js
+++ src/Pages/Main/App.js
@@ -1,10 +1,12 @@
 import "./style.css";
 import HeaderPoke from "../../Components/HeaderPoke";
+import CardPoke from "../../Components/CardPoke";
 
 function App() {
   return (
     <div className="App">
       <HeaderPoke />
+      <CardPoke />
     </div>
   );
 }
