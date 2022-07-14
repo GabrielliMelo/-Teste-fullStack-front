a6f2806d6ce3f44ef2764ccacc7c66d90cf35a86
Gabrielli5047 <gabrielli.meloo16@gmail.com>
2022-07-14 13:27:41 -0300

    test: moke array of how I will get from api

---
+++ src/Services/MokePokemons.js
@@ -0,0 +1,104 @@
+const pokemonstTeste = [
+  {
+    name: "bulbasaur",
+    species: { name: "bulbasaur" },
+    sprites: {
+      back_default:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png",
+    },
+    weight: 69,
+    height: 70,
+  },
+  {
+    name: "bulbasaur",
+    species: { name: "Seed Pokémon" },
+    sprites: {
+      back_default:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/2.png",
+    },
+    weight: 56,
+    height: 73,
+  },
+  {
+    name: "ivysaur",
+    species: { name: "Seed Pokémon" },
+    sprites: {
+      back_default:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/3.png",
+    },
+    weight: 80,
+    height: 79,
+  },
+  {
+    name: "bulbasaur",
+    species: { name: "bulbasaur" },
+    sprites: {
+      back_default:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/4.png",
+    },
+    weight: 69,
+    height: 70,
+  },
+  {
+    name: "bulbasaur",
+    species: { name: "Seed Pokémon" },
+    sprites: {
+      back_default:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/5.png",
+    },
+    weight: 56,
+    height: 73,
+  },
+  {
+    name: "ivysaur",
+    species: { name: "Seed Pokémon" },
+    sprites: {
+      back_default:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/6.png",
+    },
+    weight: 80,
+    height: 79,
+  },
+  {
+    name: "bulbasaur",
+    species: { name: "bulbasaur" },
+    sprites: {
+      back_default:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/7.png",
+    },
+    weight: 69,
+    height: 70,
+  },
+  {
+    name: "bulbasaur",
+    species: { name: "Seed Pokémon" },
+    sprites: {
+      back_default:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/8.png",
+    },
+    weight: 56,
+    height: 73,
+  },
+  {
+    name: "ivysaur",
+    species: { name: "Seed Pokémon" },
+    sprites: {
+      back_default:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/9.png",
+    },
+    weight: 80,
+    height: 79,
+  },
+  {
+    name: "bulbasaur",
+    species: { name: "Seed Pokémon" },
+    sprites: {
+      back_default:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/10.png",
+    },
+    weight: 56,
+    height: 73,
+  },
+];
+
+module.exports = pokemonstTeste;
