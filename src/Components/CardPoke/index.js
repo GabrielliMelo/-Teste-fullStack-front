9aceae896e194552c3b8880f564d8794baae5110
Gabrielli5047 <gabrielli.meloo16@gmail.com>
2022-07-14 14:12:24 -0300

    refactor: refactoring structure

--- src/Components/CardPoke/index.js
+++ src/Components/CardPoke/index.js
@@ -1,11 +1,9 @@
 import "./style.css";
-import pokeBall from "../../Assets/pokeball.png";
 
 function CardPoke({ name, species, sprintes, weight, height }) {
   return (
     <div className="cardPoke displayFlex">
       <h2>{name}</h2>
-      <img className="img-pokeball" src={pokeBall} alt="poke-ball" />
       <img className="img-poke" src={sprintes} alt="pokemon" />
       {/* especie altura peso */}
       <nav className="section-info">
--- src/Pages/Main/App.js
+++ src/Pages/Main/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./style.css";
 import HeaderPoke from "../../Components/HeaderPoke";
 import CardPoke from "../../Components/CardPoke";
@@ -7,12 +7,12 @@ import pokemonstTeste from "../../Services/MokePokemons";
 
 function App() {
   const [pokemonsData, setPokemonsData] = useState(pokemonstTeste);
-  console.log(pokemonsData);
 
   return (
     <div className="App">
       <HeaderPoke />
       <nav className="displayFlex">
+        <h1 className="btn-poke displayFlex">Anterior</h1>
         <div className="container-cards displayFlex">
           {pokemonsData.map((pokemon) => {
             return (
@@ -26,6 +26,7 @@ function App() {
             );
           })}
         </div>
+        <h1 className="btn-poke displayFlex">Proxima</h1>
       </nav>
     </div>
   );
