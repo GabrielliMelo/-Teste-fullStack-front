6e6beae7cce60aaccf97ac59ed902cef4cc2eb18
Gabrielli5047 <gabrielli.meloo16@gmail.com>
2022-07-15 01:17:16 -0300

    chore: implements Modal in app

--- src/Pages/Main/App.js
+++ src/Pages/Main/App.js
@@ -2,12 +2,15 @@ import { useEffect, useState } from "react";
 import "./style.css";
 import HeaderPoke from "../../Components/HeaderPoke";
 import CardPoke from "../../Components/CardPoke";
-import InputPoke from "../../Components/InputPoke";
 import api from "../../Services/api";
+import ModalCardPoke from "../../Components/ModalCardPoke";
 
 function App() {
   const [pokemonsDataApi, setPokemonsDataApi] = useState([]);
+  const [pokemon, setPokemon] = useState({});
   const [page, setPage] = useState(1);
+  const [name, setName] = useState("");
+  const [modalCardPoke, setModalCardPoke] = useState(false);
 
   useEffect(() => {
     const pagedListPokemon = async () => {
@@ -17,10 +20,29 @@ function App() {
     pagedListPokemon();
   }, [page]);
 
+  const pokemonInfo = async () => {
+    const response = await api.get(`pokemons/${name}`);
+    setPokemon(response.data);
+    if (response.statusText === "OK") {
+      setModalCardPoke(true);
+    }
+  };
+
+  const handleToggleModal = () => {
+    setModalCardPoke((prevState) => !prevState);
+  };
+
   return (
     <div className="App">
       <HeaderPoke />
-      <InputPoke />
+      <div className="inputPoke displayFlex">
+        <input
+          type="text"
+          placeholder="Buscar pokemon"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button onClick={pokemonInfo}>Buscar</button>
+      </div>
       <nav className="displayFlex">
         <h1
           className="btn-poke displayFlex"
@@ -29,9 +51,10 @@ function App() {
           Anterior
         </h1>
         <div className="container-cards displayFlex">
-          {pokemonsDataApi.map((pokemon) => {
+          {pokemonsDataApi.map((pokemon, index) => {
             return (
               <CardPoke
+                key={index}
                 name={pokemon.name}
                 species={pokemon.species.name}
                 sprintes={pokemon.sprites.back_default}
@@ -45,6 +68,16 @@ function App() {
           Proxima
         </h1>
       </nav>
+      <ModalCardPoke
+        abilities={pokemon.abilities}
+        name={pokemon.name}
+        species={pokemon.species ? pokemon.species.name : ""}
+        sprintes={pokemon.sprites ? pokemon.sprites.back_default : ""}
+        weight={pokemon.weight}
+        height={pokemon.height}
+        modalCardPoke={modalCardPoke}
+        handleToggleModal={handleToggleModal}
+      />
     </div>
   );
 }
--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_URL_API,
+  baseURL: process.env.REACT_APP_URL_API_LOCAL,
   timeout: 10000,
   headers: { "Content-Type": "application/json" },
 });
