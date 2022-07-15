53dd31c8a983cdd1b8c3592c1ef46a36449a93b8
Gabrielli5047 <gabrielli.meloo16@gmail.com>
2022-07-15 02:38:29 -0300

    refactor: main as router

--- src/Pages/Main/App.js
+++ src/Pages/Main/App.js
@@ -1,85 +1,15 @@
-import { useEffect, useState } from "react";
 import "./style.css";
-import HeaderPoke from "../../Components/HeaderPoke";
-import CardPoke from "../../Components/CardPoke";
-import api from "../../Services/api";
-import ModalCardPoke from "../../Components/ModalCardPoke";
+import { Route, Routes } from "react-router-dom";
+import Home from "../Home";
+import PokemonPage from "../PokemonPage";
 
-function App() {
-  const [pokemonsDataApi, setPokemonsDataApi] = useState([]);
-  const [pokemon, setPokemon] = useState({});
-  const [page, setPage] = useState(1);
-  const [name, setName] = useState("");
-  const [modalCardPoke, setModalCardPoke] = useState(false);
-
-  useEffect(() => {
-    const pagedListPokemon = async () => {
-      const response = await api.get(`pokemons?limit=10&page=${page}`);
-      setPokemonsDataApi(response.data);
-    };
-    pagedListPokemon();
-  }, [page]);
-
-  const pokemonInfo = async () => {
-    const response = await api.get(`pokemons/${name}`);
-    setPokemon(response.data);
-    if (response.statusText === "OK") {
-      setModalCardPoke(true);
-    }
-  };
-
-  const handleToggleModal = () => {
-    setModalCardPoke((prevState) => !prevState);
-  };
-
+function Main() {
   return (
-    <div className="App">
-      <HeaderPoke />
-      <div className="inputPoke displayFlex">
-        <input
-          type="text"
-          placeholder="Buscar pokemon"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <button onClick={pokemonInfo}>Buscar</button>
-      </div>
-      <nav className="displayFlex">
-        <h1
-          className="btn-poke displayFlex"
-          onClick={() => setPage(page === 1 ? 1 : page - 1)}
-        >
-          Anterior
-        </h1>
-        <div className="container-cards displayFlex">
-          {pokemonsDataApi.map((pokemon, index) => {
-            return (
-              <CardPoke
-                key={index}
-                name={pokemon.name}
-                species={pokemon.species.name}
-                sprintes={pokemon.sprites.back_default}
-                weight={pokemon.weight}
-                height={pokemon.height}
-              />
-            );
-          })}
-        </div>
-        <h1 className="btn-poke displayFlex" onClick={() => setPage(page + 1)}>
-          Proxima
-        </h1>
-      </nav>
-      <ModalCardPoke
-        abilities={pokemon.abilities}
-        name={pokemon.name}
-        species={pokemon.species ? pokemon.species.name : ""}
-        sprintes={pokemon.sprites ? pokemon.sprites.back_default : ""}
-        weight={pokemon.weight}
-        height={pokemon.height}
-        modalCardPoke={modalCardPoke}
-        handleToggleModal={handleToggleModal}
-      />
-    </div>
+    <Routes>
+      <Route path="/home" element={<Home />} exact />
+      <Route path="/pokemons/:pokemon" element={<PokemonPage />} />
+    </Routes>
   );
 }
 
-export default App;
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "../src/Pages/Main/App";
+import App from "./Pages/Main/App";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
